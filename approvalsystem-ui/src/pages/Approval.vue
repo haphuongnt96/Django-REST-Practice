d1467b506e978e3b1929cf2edc986433c51c7c33
MisterHamilton <phannt@techvify.com.vn>
2021-09-09 08:18:07 +0700

    fix: update approval UI

--- approvalsystem-ui/src/pages/Approval.vue
+++ approvalsystem-ui/src/pages/Approval.vue
@@ -3,202 +3,192 @@
     <NavBar/>
     <div class="container-fluid my-2">
         <div class="row">
-            <div class="col-1">
-                <div class="applicant">
-                    <div class="top-label">
-                        <label>申請者</label>
+            <div class="col-10 border-right">
+                <div class="row">
+                    <div class="col-12 text-left">
+                        <h4>承認状況：00001</h4>
                     </div>
-                    <div class="grid">
-                        <label>武田 信玄</label>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 副支店長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label current-level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-danger"><i class="fa fa-times-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-1">
-                    <div class="row px-1">
-                        <div class="col-8 border border-dark bg-info">
-                            コメント
-                        </div>
-                        <div class="col-4 border border-dark">
-                            有
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label">
+                                <label>申請者</label>
+                            </div>
+                            <div class="grid">
+                                <label>武田 信玄</label>
+                            </div>
                         </div>
                     </div>
-                    <button class="my-1 btn btn-square-pop-orange-long block">通知者</button>
-                    <button class="my-1 btn btn-square-pop-red-long block">複写機能</button>
-            </div>
-        </div>
-        <hr/>
-        <div class="row">
-            <div class="col-1">
-                <div class="applicant">
-                    <div class="top-label">
-                        <label>申請者</label>
-                    </div>
-                    <div class="grid">
-                        <label>武田 信玄</label>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 副支店長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label current-level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"><label><span class="text-danger"><i class="fa fa-times-circle"></i></span> 09/13/2021</label></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="applicant">
-                    <div class="top-label level m-0">
-                        <label>大阪北支店 課長</label>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>山田 太郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                    <div class="row m-0">
-                        <div class="col-6 grid"><label>鈴木 一郎</label></div>
-                        <div class="col-6 grid"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-1">
-                    <div class="row px-1">
-                        <div class="col-8 border border-dark bg-info">
-                            コメント
-                        </div>
-                        <div class="col-4 border border-dark">
-                            有
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>大阪北支店 課長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>山田 太郎</label></div>
+                                <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>鈴木 一郎</label></div>
+                                <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
+                            </div>
                         </div>
                     </div>
-                    <button class="my-1 btn btn-square-pop-orange-long block">通知者</button>
-                    <button class="my-1 btn btn-square-pop-red-long block">複写機能</button>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>大阪北支店 副支店長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>武藤 敬司</label></div>
+                                <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>蝶野 正洋</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label current-level m-0">
+                                <label>大阪北支店 支店長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>長州 力</label></div>
+                                <div class="col-6 grid"><label><span class="text-danger"><i class="fa fa-times-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>経営企画部</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>小橋 建太</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>三沢 光晴</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>経営企画部 課長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>山田 太郎</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>鈴木 一郎</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-12 text-left">
+                        <h4>承認状況：000010</h4>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label">
+                                <label>申請者</label>
+                            </div>
+                            <div class="grid">
+                                <label>武田 信玄</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>大阪北支店 課長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>山田 太郎</label></div>
+                                <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>鈴木 一郎</label></div>
+                                <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>大阪北支店 副支店長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>武藤 敬司</label></div>
+                                <div class="col-6 grid"><label><span class="text-success"><i class="fa fa-check-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>蝶野 正洋</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label current-level m-0">
+                                <label>大阪北支店 支店長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>長州 力</label></div>
+                                <div class="col-6 grid"><label><span class="text-danger"><i class="fa fa-times-circle"></i></span> 09/13/2021</label></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>経営企画部</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>小橋 建太</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>三沢 光晴</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="applicant">
+                            <div class="top-label level m-0">
+                                <label>経営企画部 課長</label>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>山田 太郎</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-6 grid"><label>鈴木 一郎</label></div>
+                                <div class="col-6 grid"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="row px-2 my-5 justify-content-center">
+                    <div class="col-4 border border-dark bg-info">
+                        コメント
+                    </div>
+                    <div class="col-2 border border-dark">
+                        有
+                    </div>
+                </div>
+                <button class="my-5 btn btn-square-pop-orange-long">通知者</button><br/>
+                <button class="my-5 btn btn-square-pop-red-long">複写機能</button>
             </div>
         </div>
     </div>
