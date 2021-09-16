40ca348698a72ce9874dd51ab73d121cad46de94
HyunwookPark <park.hyunwook@planaria.co.jp>
2021-09-16 09:27:01 +0900

    別スキーマとユーザ生成は削除し環境変数設定も修正

--- initdb/01_init.sql
+++
@@ -1,4 +0,0 @@
-CREATE ROLE dev_user WITH LOGIN PASSWORD 'devdev';
-CREATE SCHEMA approval AUTHORIZATION dev_user;
--- default schema for search_path
-ALTER ROLE dev_user SET search_path TO approval;
