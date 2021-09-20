ed1ffb079fdeeebaa4baff70237d718ad1c2a700
MisterHamilton <phannt@techvify.com.vn>
2021-09-20 09:55:53 +0700

    [FIX] fix settings db

--- approvalsystem/settings/db.py
+++ approvalsystem/settings/db.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USERNAME'),
+        'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
