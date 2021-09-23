ada396e3bd55fe202a09bb7ebe5d47bf56825bfc
MisterHamilton <phannt@techvify.com.vn>
2021-09-23 19:43:34 +0700

    [FIX] fix json renderer

--- approvalsystem/utils/renderers.py
+++ approvalsystem/utils/renderers.py
@@ -6,8 +6,10 @@ from rest_framework.renderers import JSONRenderer
 class CustomJsonRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         status_code = renderer_context['response'].status_code
-        message = data.pop('detail', None)
+        message = None
         errors = None
+        if isinstance(data, dict) and 'detail' in data:
+            message = data.pop('detail')
         if status_code == status.HTTP_400_BAD_REQUEST:
             errors = data
             data = {}
