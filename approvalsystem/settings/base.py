2dc5ee290a06181c984a799608d16db28e7b189a
uranishi <uranishi.tomoaki@planaria.co.jp>
2021-10-22 18:04:02 +0900

    ログ出力の仕組みを実装

--- approvalsystem/settings/base.py
+++ approvalsystem/settings/base.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'utils.middlewares.log_middleware.LogMiddleware',
 ]
 
 # Password validation
