ccce97a1abe548ddd6a768e364ea052d9c62bf6a
HyunwookPark <park.hyunwook@planaria.co.jp>
2021-10-27 10:24:05 +0900

    conflict解消失敗部分訂正

--- approvalsystem/core/urls.py
+++ approvalsystem/core/urls.py
@@ -4,7 +4,7 @@ from .views import (
     ApprovalRouteListAPI,
     CountSummaryApprovalRouteDetailAPI,
     ApprovalRequestAPI,
-    ApprovalRouteCommentListAPI
+    ApprovalRouteCommentListAPI,
     RequestGetListAPI,
 )
 
@@ -14,12 +14,10 @@ urlpatterns = [
     path('requests/<int:request_id>/approval',
          ApprovalRequestAPI.as_view(), name='request.approval'),
     path('approval_route_details/summary/count',
-         CountSummaryApprovalRouteDetailAPI.as_view(),
-         name='approval_route_details.summary.count'),
+         CountSummaryApprovalRouteDetailAPI.as_view(), name='approval_route_details.summary.count'),
     path('approval_routes/<int:approval_route_id>/approval_route_comment',
-          ApprovalRouteCommentListAPI.as_view(), name='request.approval_route_comment')
+          ApprovalRouteCommentListAPI.as_view(), name='request.approval_route_comment'),
     path('requests/get_list',
-          RequestGetListAPI.as_view(),
-          name='requests_list'
+          RequestGetListAPI.as_view(), name='requests_list'
     )
 ]
