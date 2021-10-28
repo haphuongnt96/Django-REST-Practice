061c38902fe7addb6c37f9dde9e483b864f69da0
MisterHamilton <phannt@techvify.com.vn>
2021-10-28 09:33:02 +0700

    create authen module vuex

--- approvalsystem-ui/src/static/pageContent.ts
+++ approvalsystem-ui/src/static/pageContent.ts
@@ -155,6 +155,11 @@ const DASHBOARD = {
   POPUP_SEARCH_OBJ_INCHARGE: '担当者',
   BUTTON_CLEAR: 'クリア'
 }
+
+const APPLICATION_SEARCH_POPUP = {
+  APP_DEPARTMENT: '申請部署',
+  USER_DEPARTMENTS_BELONGED: 'ログイン者の部署'
+}
 const $pageContents = {
   APP_SIDE_BAR,
   APP_HEADER,
@@ -162,7 +167,8 @@ const $pageContents = {
   AUTHEN,
   DASHBOARD,
   APPROVAL_TABS,
-  COMMON
+  COMMON,
+  APPLICATION_SEARCH_POPUP
 }
 
 export default $pageContents
--- approvalsystem-ui/src/store/index.ts
+++ approvalsystem-ui/src/store/index.ts
@@ -1,11 +1,30 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { ActionTree } from 'vuex'
+import { authStore } from '@/store/modules/auth/authStore'
+import VuexPersistence from 'vuex-persist'
+import { Actions, State } from './storeD'
+import { Decorators as AuthDecorators } from '@/store/modules/auth/authD'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  reducer: (state: State) => ({
+    auth: state.auth
+  }),
+  supportCircular: true
+})
 
 Vue.use(Vuex)
 
+const actions: ActionTree<State, State> = {
+  [Actions.initApp]({ dispatch }) {
+    dispatch(AuthDecorators.fetchCurrentUserExt)
+  }
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  actions,
+  plugins: [vuexLocal.plugin],
+  modules: {
+    auth: authStore
+  }
 })
