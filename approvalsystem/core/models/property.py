893a4d7e4dab1473e72e168b2b4c64594a9377db
koyama <k-koyama@kintetsu-community.co.jp>
2021-11-01 17:19:39 +0900

    物件検索修正

--- approvalsystem/core/models/property.py
+++ approvalsystem/core/models/property.py
@@ -10,12 +10,15 @@ class Property(BaseModel):
     """
     property_id = models.IntegerField(primary_key=True)
     property_nm = models.CharField(max_length=50)
-    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-    segment_id = models.ForeignKey(Segment, on_delete=models.CASCADE)
-    division_id = models.ForeignKey(Division, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    segment = models.ForeignKey(Segment, on_delete=models.CASCADE)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
-    emp_cd = models.ForeignKey(User, on_delete=models.CASCADE)
+    emp = models.ForeignKey(User, on_delete=models.CASCADE)
     tel_number = models.CharField(max_length=13)
+
+    class Meta:
+        db_table = 'm_property'
 
     def __str__(self):
         return self.property_nm
