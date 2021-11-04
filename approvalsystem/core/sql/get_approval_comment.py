ecc24b32557e0ddf6a6797239cb0e10ac63230ed
MisterHamilton <phannt@techvify.com.vn>
2021-11-04 22:46:00 +0700

    [UPDATE] update get_approval_comment

--- approvalsystem/core/sql/get_approval_comment.py
+++ approvalsystem/core/sql/get_approval_comment.py
@@ -4,14 +4,14 @@ from ..models.approval_route_comment import ApprovalRouteComment
 def get_approval_comment(approval_route_id):
 	queryset = ApprovalRouteComment.objects.raw('''
 		SELECT
-			comment.*, emp.emp_nm, route.approval_type_cd
+			comment.*, emp.emp_nm, route.approval_type_id
 		FROM
 			t_approval_route_comment as comment
 		INNER JOIN
 			m_emp as emp ON emp.id=comment.ins_emp_id
 		INNER JOIN
 			t_approval_route as route
-			ON route.request_id_id=comment.request_id AND route.approval_route_id=comment.approval_route_id
+			ON route.request_id=comment.request_id AND route.approval_route_id=comment.approval_route_id
 		WHERE
 			comment.approval_route_id=%(approval_route_id)s
 		ORDER BY
